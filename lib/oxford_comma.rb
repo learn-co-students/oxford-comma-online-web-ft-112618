8c5a1fb97764cb6fb9b3c99de4e700a220dfea90
Adam Dudley <dudleyadam@gmail.com>
2018-11-26 19:09:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ") | fruit |
+  elsif 2 < array.length
+    array[-1].insert(0, "and")
+    binding.pry
+  end
 end
