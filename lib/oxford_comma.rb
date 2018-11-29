27a6bb2c48d310918529ec398104df0d531feac7
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-29 23:40:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  stringElement = ''
+  if(array.length == 1)
+    return array.join()
+  elsif(array.length == 2)
+    return array.join(" and ")
+  elsif(array.length > 2)
+    array.each do |element|
+      if(element != array[-1])
+        stringElement << element << ', '
+      else
+        stringElement << 'and ' << element
+      end
+    end
+    return stringElement
+  end
 end
