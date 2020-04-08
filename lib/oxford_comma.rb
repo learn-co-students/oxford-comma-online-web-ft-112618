4c2dbb06e13dae07aef58bb7ecee4a8c18260d16
Grace Berman <grace.g.berman@gmail.com>
2020-04-08 21:44:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    # array.join(", ") << "and"
-    new_array = array
-    new_array[-1] = "and #{array.last}"
-    new_array.join(", ")
+    array[-1].insert(0, "and ")
+    array.join(", ")
   end
 end
